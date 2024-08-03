6cdc7eaaef945eb629ba689f7f60713641977334
Ahmet Harun Ertürk <is_ahmet@outlook.com>
2024-08-03 21:43:52 +0200

    filter and advanced filter with backend

---
+++ filter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import { Users } from "./users"
+import Table from './Table'
+
+function App() {
+  const [query, setQuery] = useState("")
+
+  const keys = ["first_name", "last_name", "email"]
+
+  //console.log(Users[0]["first_name"])
+
+  const search = (data) => {
+    return data.filter(item =>
+      keys.some(key =>
+        item[key].toLowerCase().includes(query.toLowerCase())
+      )
+    )
+  }
+
+  return (
+    <>
+      <div className="app">
+        <input type="text" placeholder='search..' className="search" onChange={e => setQuery(e.target.value)} />
+
+        <Table data={search(Users)} />
+      </div>
+    </>
+  )
+}
+
+export default App
